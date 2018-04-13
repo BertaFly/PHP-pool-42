11d4370b4445fff69a55bcee2f5aa99d8d9c5fd6
Berta Fly <inovykov@e2r11p5.unit.ua>
2018-04-13 19:04:05 +0300

    to do

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -22,28 +22,55 @@ function addTask() {
 		var li = document.createElement("li");
 		// var node = document.createTextNode(tmp, {"id": "toDoTask", "name": taskNbr, "onclick": "delTask('name.value')"});
 		var node = document.createTextNode(tmp);
-		
 		li.appendChild(node);
 		toDo.insertBefore(li, toDo.firstChild);
-		var addOnClick = document.getElementsByTagName("div");
-		var att = document.createAttribute("onclick");
-		att.value = "delTask(name.value)";
-		addOnClick.setAttributeNode(att);
-		var att = document.createAttribute("name");
-		att.value = taskNbr;
-		addOnClick.setAttributeNode(att);
-		taskNbr++;
+
+		// var addOnClick = document.getElementsByTagName("li");
+		// var att = document.createAttribute("onclick");
+		// att.value = "delTask(name.value)";
+		// addOnClick.setAttributeNode(att);
+		// var att = document.createAttribute("name");
+		// att.value = taskNbr;
+		// addOnClick.setAttributeNode(att);
+		// taskNbr++;
+
+		document.getElementById("ft_list").appendChild(li);
+	    li.setAttribute("name", taskNbr++);
+	    li.setAttribute("onclick", "delTask(3)");
 	}
 };
 
 function delTask(elem) {
-	var res = confirm("After click ok you will delete this task. Are you sure?");
-	if (res === TRUE)
+	confirm("After click ok you will delete this task. Are you sure?");
+	var container = document.getElementById("ft_list");
+	var del = container.getElementsByTagName("li");
+	var myArr = [];
+	for (var i = 0; i < del.length; i++)
 	{
-		var toDell = document.getElementsByName('elem');
-    	// this.parentElement.removeChild(this);
-    	toDell.remove();
+		var self = del[i];
+		myArr.push(self);
 	}
+	console.log(myArr);
+	// del.forEach(function(elem) {
+	// 	if (strpos(del['outerHTML'], "elem") != NULL)
+	// 	{
+	// 		del.remove();
+	// 		break ;
+	// 	}
+	// });
+	for (var i = 0; i < myArr.length; i++) {
+		console.log(myArr[i]['outerHTML'].indexOf('name="' + elem));
+    	if (myArr[i]['outerHTML'].indexOf(elem) + 1 !== 0)
+		{
+			del[i].remove();
+			break ;
+		}
+	}
+	// del[0].remove();
+	console.log(del);
+    	// container.parentElement.removeChild(del[0]);
+    	// toDell.remove();
+	
 };
 // function delTask(elem) {
 // 	// var toDo = document.getElementById('ft_list');
